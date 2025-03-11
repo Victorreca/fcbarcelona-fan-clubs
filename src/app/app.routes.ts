69b85b2549ebedb3283382b3a200dfb8bd45312c
Victor Redondo <vrviktor@gmail.com>
2025-03-11 17:55:03 +0100

    Add header with its components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'charts', component: ChartsComponent },
+];
