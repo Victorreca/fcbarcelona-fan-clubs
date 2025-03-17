89c9e356d1c15b5a3c3003c1f1f43303ab863623
Victor Redondo <vrviktor@gmail.com>
2025-03-17 14:16:01 +0100

    Add loader

--- frontend/src/app/components/list-fanclubs/list-fanclubs.component.ts
+++ frontend/src/app/components/list-fanclubs/list-fanclubs.component.ts
@@ -3,15 +3,17 @@ import { FanClub } from '../../interfaces/fanclub';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { FanclubService } from '../../services/fanclub.service';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-list-fanclubs',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, LoaderComponent],
   templateUrl: './list-fanclubs.component.html',
   styleUrl: './list-fanclubs.component.scss',
 })
 export class ListFanclubsComponent implements OnInit {
   listFanclubs: FanClub[] = [];
+  loading: boolean = false;
 
   private fanClubService = inject(FanclubService);
 
@@ -20,8 +22,10 @@ export class ListFanclubsComponent implements OnInit {
   }
 
   getListFanClubs() {
+    this.loading = true;
     this.fanClubService.getListFanClubs().subscribe((data) => {
       this.listFanclubs = data;
+      this.loading = false;
     });
   }
 }
