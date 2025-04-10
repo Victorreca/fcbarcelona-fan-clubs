6a3d1374bc4f493592560a3b0382554235fbf3ae
Victor Redondo <vrviktor@gmail.com>
2025-04-10 11:18:08 +0200

    Fix environment variables in server and connection

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "fanclub_barcelona",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
   {
-    host: process.env.DB_HOST || "localhost",
-    dialect: (process.env.DB_DIALECT as any) || "mysql",
+    host: process.env.DB_HOST as string,
+    dialect: process.env.DB_DIALECT as any,
   }
 );
 
--- backend/src/models/server.ts
+++ backend/src/models/server.ts
@@ -10,7 +10,7 @@ class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || "3000";
+    this.port = process.env.PORT as string;
     this.listen();
     this.middlewares();
     this.routes();
